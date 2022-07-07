95a0a15fc423577a39fbc7e437a964e597318ab7
Lucas <ldantascruz@gmail.com>
2022-07-07 16:29:15 -0300

    finish app imc

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = 'Informe seus dados!';
+
+  void _resetFields() {
+    setState(() {
+      weightController.text = '';
+      heightController.text = '';
+      _infoText = 'Informe seus dados!';
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = 'Abaixo do peso (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = 'Peso ideal (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = 'Levemente acima do peso (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = 'Obesidade grau 1 (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = 'Obesidade grau 2 (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 40) {
+        _infoText = 'Obesidade grau 3 (${imc.toStringAsPrecision(3)})';
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,70 +63,92 @@ class _HomeState extends State<Home> {
         backgroundColor: const Color(0xff18E300),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh)),
+          IconButton(
+            onPressed: _resetFields,
+            icon: const Icon(Icons.refresh),
+          ),
         ],
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            const Icon(
-              Icons.person_outline,
-              size: 120,
-              color: Color(0xff18E300),
-            ),
-            const TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Peso (Kg)",
-              ),
-              textAlign: TextAlign.center,
-              style: TextStyle(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const Icon(
+                Icons.person_outline,
+                size: 120,
                 color: Color(0xff18E300),
-                fontSize: 25,
               ),
-            ),
-            const TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Altura (cm)",
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: "Peso (Kg)",
+                ),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Color(0xff18E300),
+                  fontSize: 25,
+                ),
+                controller: weightController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Insira seu peso!!';
+                  }
+                },
               ),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xff18E300),
-                fontSize: 25,
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: "Altura (cm)",
+                ),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Color(0xff18E300),
+                  fontSize: 25,
+                ),
+                controller: heightController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Insira sua altura!!';
+                  }
+                },
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: const Color(0xff18E300),
-                        padding: const EdgeInsets.all(14),
-                        //fixedSize: const Size(300, 50),
-                      ),
-                      onPressed: () {},
-                      child: const Text(
-                        "Calcular",
-                        style: TextStyle(fontSize: 25),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color(0xff18E300),
+                          padding: const EdgeInsets.all(14),
+                          //fixedSize: const Size(300, 50),
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            calculate();
+                          }
+                        },
+                        child: const Text(
+                          "Calcular",
+                          style: TextStyle(fontSize: 25),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const Text(
-              "Info",
-              style: TextStyle(
-                color: Color(0xff18E300),
-                fontSize: 25,
-              ),
-              textAlign: TextAlign.center,
-            )
-          ],
+              Text(
+                _infoText,
+                style: const TextStyle(
+                  color: Color(0xff18E300),
+                  fontSize: 25,
+                ),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
         ),
       ),
     );
